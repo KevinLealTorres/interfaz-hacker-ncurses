b0cbc3da40702a820ed8098753deea13b81d0335
antonio muro <example@example.com>
2025-02-26 16:33:34 -0600

    se estandarizó a C++11

--- main.cpp
+++ main.cpp
@@ -3,31 +3,10 @@
 #include <thread>
 #include <time.h>
 #include <random>
-
-#define std_colors 1
-#define skull_colors 2
-#define text_bar_colors 3
-#define panel_colors 4
-#define bar_colors 5
-#define alert_colors 6
-
-char * palabras_clave[10] = {"Tecmilenio","Servidores VPN","ARPs","firewalls","compilador","CND","BBVA","ALIENS","FBI","SI"};
-
-int main()
-{
-    srand((unsigned) time(NULL));
-
-    initscr();
-    start_color();
-    init_pair(std_colors, COLOR_WHITE, COLOR_BLACK);
-    init_pair(skull_colors, COLOR_RED, COLOR_BLACK);
-    init_pair(text_bar_colors, COLOR_WHITE, COLOR_YELLOW);
-    init_pair(panel_colors,COLOR_GREEN, COLOR_BLACK);
-    init_pair(bar_colors, COLOR_WHITE, COLOR_BLACK);
-    bkgd(COLOR_PAIR(std_colors));
+#include <vector>
 
 
-    const char * skull_ASCII =
+const char * skull_ASCII =
 "                 uuuuuuu                      \n"
 "             uu$$$$$$$$$$$uu                  \n"
 "          uu$$$$$$$$$$$$$$$$$uu               \n"
@@ -53,7 +32,29 @@ int main()
 "  u$$$uuu$$$$$$$$$uu \"\"$$$$$$$$$$$uuu$$$    \n"
 "  $$$$$$$$$$\"\"\"\"           \"\"$$$$$$$$$$$\n"
 "   \"$$$$$\"                      \"\"$$$$\"\"\n"
-"     $$$\"                         $$$$\"      ";
+"     $$$\"                         $$$$\"       ";
+
+constexpr short std_colors = 1;
+constexpr short skull_colors = 2;
+constexpr short text_bar_colors = 3;
+constexpr short bar_colors = 4;
+constexpr short panel_colors = 5;
+//constexpr short alert_colors = 6;
+
+const auto * palabras_clave = new std::vector<const char*> {"Tecmilenio","Servidores VPN","ARPs","firewalls","compilador","CND","BBVA","ALIENS","FBI","SI"};
+
+int main()
+{
+    srand((unsigned) time(NULL));
+
+    initscr();
+    start_color();
+    init_pair(std_colors, COLOR_WHITE, COLOR_BLACK);
+    init_pair(skull_colors, COLOR_RED, COLOR_BLACK);
+    init_pair(text_bar_colors, COLOR_WHITE, COLOR_YELLOW);
+    init_pair(panel_colors,COLOR_GREEN, COLOR_BLACK);
+    init_pair(bar_colors, COLOR_WHITE, COLOR_BLACK);
+    bkgd(COLOR_PAIR(std_colors));
 
     //bar configs
     WINDOW * bar = newwin(3, COLS, LINES-3, 0);
@@ -69,18 +70,18 @@ int main()
     wbkgd(panel, COLOR_PAIR(panel_colors));
     box(panel,0,0);
 
-    wprintw(skull,"%s", (char*) skull_ASCII);
+    wprintw(skull,"%s", skull_ASCII);
     wprintw(panel,"Registros de propagación del ransomware ");
 
     std::thread bar_filling([&]() -> void {
         wmove(bar, 1,1);
-        int contador = 0;
-        while(contador++ < COLS - 2) {
+
+        for (short i = 0; i < COLS - 2; i++) {
             refresh();
             wrefresh(panel);
             wrefresh(skull);
             wrefresh(bar);
-            std::this_thread::sleep_for(std::chrono::duration<float>((float) (rand() % 5) / 10));
+            std::this_thread::sleep_for(std::chrono::duration<float>((rand() % 5) / 10.0f));
             wattron(bar,COLOR_PAIR(text_bar_colors));
             wprintw(bar, " ");
             wattroff(bar, COLOR_PAIR(text_bar_colors));
@@ -89,9 +90,9 @@ int main()
 
     wmove(panel, 1,2);
 
-    for(int i = 0; i < 10; i++) {
+    for(short i = 0; i < 10; i++) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        wprintw(panel, "hackeando %s... ", (char*) palabras_clave[i]);
+        wprintw(panel, "hackeando %s... ", palabras_clave->at(i));
         wmove(panel, i+1,2);
     }
 

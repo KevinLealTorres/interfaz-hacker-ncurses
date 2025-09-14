7f91b0050a199836ef8be8dbb614fce395f0ae7d
antonio muro <example@example.com>
2025-09-13 23:51:39 -0600

    se formateó el archivo y se arreglaron valores que debieron ser constantes

--- main.cpp
+++ main.cpp
@@ -5,95 +5,96 @@
 #include <random>
 #include <vector>
 
+typedef unsigned short ushort;
 
-const char * skull_ASCII =
-"                 uuuuuuu                      \n"
-"             uu$$$$$$$$$$$uu                  \n"
-"          uu$$$$$$$$$$$$$$$$$uu               \n"
-"         u$$$$$$$$$$$$$$$$$$$$$u              \n"
-"        u$$$$$$$$$$$$$$$$$$$$$$$u             \n"
-"       u$$$$$$$$$$$$$$$$$$$$$$$$$u            \n"
-"       u$$$$$$$$$$$$$$$$$$$$$$$$$u            \n"
-"       u$$$$$$\"   \"$$$\"   \"$$$$$$u        \n"
-"       \"$$$$\"      u$u       $$$$\"         \n"
-"        $$$u       u$u       u$$$             \n"
-"        $$$u      u$$$u      u$$$             \n"
-"         \"$$$$uu$$$   $$$uu$$$$\"            \n"
-"          \"$$$$$$$\"   \"$$$$$$$\"           \n"
-"            u$$$$$$$u$$$$$$$u                 \n"
-"             u$\"$\"$\"$\"$\"$\"$u            \n"
-"  uuu        $$u$ $ $ $ $u$$       uuu        \n"
-" u$$$$        $$$$$u$u$u$$$       u$$$$       \n"
-"  $$$$$uu      \"$$$$$$$$$\"     uu$$$$$$     \n"
-"u$$$$$$$$$$$uu    \"\"\"\"\"    uuuu$$$$$$$$$$\n"
-"$$$$\"\"\"$$$$$$$$$$uuu   uu$$$$$$$$$\"\"\"$$$\n"
-" \"\"\"      \"\"$$$$$$$$$$$uu \"\"$\"\"\"    \n"
-"           uuuu \"\"$$$$$$$$$$uuu             \n"
-"  u$$$uuu$$$$$$$$$uu \"\"$$$$$$$$$$$uuu$$$    \n"
-"  $$$$$$$$$$\"\"\"\"           \"\"$$$$$$$$$$$\n"
-"   \"$$$$$\"                      \"\"$$$$\"\"\n"
-"     $$$\"                         $$$$\"       ";
+const char *SKULL_ASCII =
+    "                 uuuuuuu                      \n"
+    "             uu$$$$$$$$$$$uu                  \n"
+    "          uu$$$$$$$$$$$$$$$$$uu               \n"
+    "         u$$$$$$$$$$$$$$$$$$$$$u              \n"
+    "        u$$$$$$$$$$$$$$$$$$$$$$$u             \n"
+    "       u$$$$$$$$$$$$$$$$$$$$$$$$$u            \n"
+    "       u$$$$$$$$$$$$$$$$$$$$$$$$$u            \n"
+    "       u$$$$$$\"   \"$$$\"   \"$$$$$$u        \n"
+    "       \"$$$$\"      u$u       $$$$\"         \n"
+    "        $$$u       u$u       u$$$             \n"
+    "        $$$u      u$$$u      u$$$             \n"
+    "         \"$$$$uu$$$   $$$uu$$$$\"            \n"
+    "          \"$$$$$$$\"   \"$$$$$$$\"           \n"
+    "            u$$$$$$$u$$$$$$$u                 \n"
+    "             u$\"$\"$\"$\"$\"$\"$u            \n"
+    "  uuu        $$u$ $ $ $ $u$$       uuu        \n"
+    " u$$$$        $$$$$u$u$u$$$       u$$$$       \n"
+    "  $$$$$uu      \"$$$$$$$$$\"     uu$$$$$$     \n"
+    "u$$$$$$$$$$$uu    \"\"\"\"\"    uuuu$$$$$$$$$$\n"
+    "$$$$\"\"\"$$$$$$$$$$uuu   uu$$$$$$$$$\"\"\"$$$\n"
+    " \"\"\"      \"\"$$$$$$$$$$$uu \"\"$\"\"\"    \n"
+    "           uuuu \"\"$$$$$$$$$$uuu             \n"
+    "  u$$$uuu$$$$$$$$$uu \"\"$$$$$$$$$$$uuu$$$    \n"
+    "  $$$$$$$$$$\"\"\"\"           \"\"$$$$$$$$$$$\n"
+    "   \"$$$$$\"                      \"\"$$$$\"\"\n"
+    "     $$$\"                         $$$$\"       ";
 
-constexpr short std_colors = 1;
-constexpr short skull_colors = 2;
-constexpr short text_bar_colors = 3;
-constexpr short bar_colors = 4;
-constexpr short panel_colors = 5;
-//constexpr short alert_colors = 6;
+constexpr ushort STD_COLORS = 1,
+                 SKULL_COLORS = 2,
+                 TEXT_BAR_COLORS = 3,
+                 BAR_COLORS = 4,
+                 PANEL_COLORS = 5;
 
-const auto * palabras_clave = new std::vector<const char*> {"Tecmilenio","Servidores VPN","ARPs","firewalls","compilador","CND","BBVA","ALIENS","FBI","SI"};
+const std::vector<std::string> PALABRAS_CLAVE{"Tecmilenio", "Servidores VPN", "ARPs", "firewalls", "compilador", "CND", "BBVA", "ALIENS", "FBI", "SI"};
 
 int main()
 {
-    srand((unsigned) time(NULL));
+    srand((unsigned)time(nullptr));
 
     initscr();
     start_color();
-    init_pair(std_colors, COLOR_WHITE, COLOR_BLACK);
-    init_pair(skull_colors, COLOR_RED, COLOR_BLACK);
-    init_pair(text_bar_colors, COLOR_WHITE, COLOR_YELLOW);
-    init_pair(panel_colors,COLOR_GREEN, COLOR_BLACK);
-    init_pair(bar_colors, COLOR_WHITE, COLOR_BLACK);
-    bkgd(COLOR_PAIR(std_colors));
+    init_pair(STD_COLORS, COLOR_WHITE, COLOR_BLACK);
+    init_pair(SKULL_COLORS, COLOR_RED, COLOR_BLACK);
+    init_pair(TEXT_BAR_COLORS, COLOR_WHITE, COLOR_YELLOW);
+    init_pair(PANEL_COLORS, COLOR_GREEN, COLOR_BLACK);
+    init_pair(BAR_COLORS, COLOR_WHITE, COLOR_BLACK);
+    bkgd(COLOR_PAIR(STD_COLORS));
 
-    //bar configs
-    WINDOW * bar = newwin(3, COLS, LINES-3, 0);
-    wbkgd(bar, COLOR_PAIR(bar_colors));
-    box(bar, 0,0);
+    // bar configs
+    WINDOW *bar = newwin(3, COLS, LINES - 3, 0);
+    wbkgd(bar, COLOR_PAIR(BAR_COLORS));
+    box(bar, 0, 0);
 
-    //skull configs
-    WINDOW * skull = newwin(26, 48, 0,0);
-    wbkgd(skull, COLOR_PAIR(skull_colors));
+    // skull configs
+    WINDOW *skull = newwin(26, 48, 0, 0);
+    wbkgd(skull, COLOR_PAIR(SKULL_COLORS));
 
-    //panel configs
-    WINDOW * panel = newwin(LINES-3, COLS-49, 0, 49);
-    wbkgd(panel, COLOR_PAIR(panel_colors));
-    box(panel,0,0);
+    // panel configs
+    WINDOW *panel = newwin(LINES - 3, COLS - 49, 0, 49);
+    wbkgd(panel, COLOR_PAIR(PANEL_COLORS));
+    box(panel, 0, 0);
 
-    wprintw(skull,"%s", skull_ASCII);
-    wprintw(panel,"Registros de propagación del ransomware ");
+    wprintw(skull, "%s", SKULL_ASCII);
+    wprintw(panel, "Registros de propagación del ransomware ");
 
-    std::thread bar_filling([&]() -> void {
+    std::thread bar_filling([&]() -> void
+                            {
         wmove(bar, 1,1);
 
-        for (short i = 0; i < COLS - 2; i++) {
+        for (ushort i = 0; i < COLS - 2; i++) {
             refresh();
             wrefresh(panel);
             wrefresh(skull);
             wrefresh(bar);
             std::this_thread::sleep_for(std::chrono::duration<float>((rand() % 5) / 10.0f));
-            wattron(bar,COLOR_PAIR(text_bar_colors));
+            wattron(bar,COLOR_PAIR(TEXT_BAR_COLORS));
             wprintw(bar, " ");
-            wattroff(bar, COLOR_PAIR(text_bar_colors));
-        }
-    });
+            wattroff(bar, COLOR_PAIR(TEXT_BAR_COLORS));
+        } });
 
-    wmove(panel, 1,2);
+    wmove(panel, 1, 2);
 
-    for(short i = 0; i < 10; i++) {
+    for (ushort i = 0; i < 10; i++)
+    {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        wprintw(panel, "hackeando %s... ", palabras_clave->at(i));
-        wmove(panel, i+1,2);
+        wprintw(panel, "hackeando %s... ", PALABRAS_CLAVE.at(i).c_str());
+        wmove(panel, i + 1, 2);
     }
 
     bar_filling.join();
